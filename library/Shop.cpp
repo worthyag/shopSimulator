d28a8cd1547465be0d7e48948e303b6ea881dfb2
Worthy <worthyintech@gmail.com>
2024-07-05 11:21:30 +0100

    Implemented the buildItemsDB logic.

--- library/Shop.cpp
+++ library/Shop.cpp
@@ -1,5 +1,9 @@
 #include "Shop.hpp"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 void Shop::welcomeUser() {
   std::cout << "Welcome to Worthy's Shop." << std::endl;
 
@@ -7,8 +11,43 @@ void Shop::welcomeUser() {
   displayItems();
 }
 
+std::vector<std::vector<std::string>> Shop::buildItemsDB() {
+  std::ifstream ifs {};
+  std::string itemInfoStr {};
+
+  // Building the items DB.
+  std::vector<std::vector<std::string>> itemsDB {};
+  
+  ifs.open("../items/shopItems.csv");
+
+  while (std::getline(ifs, itemInfoStr)) {
+    std::vector<std::string> itemInfo {};
+    std::stringstream ss {itemInfoStr};
+    std::string value {};
+
+    while (std::getline(ss, value, ',')) {
+      itemInfo.push_back(value);
+    }
+
+    itemsDB.push_back(itemInfo);
+  }
+
+  ifs.close();
+  return itemsDB;
+}
+
 void Shop::displayItems() {
   std::cout << "Display items." << std::endl;
+
+  std::vector<std::vector<std::string>> itemsDB {buildItemsDB()};
+
+  // Testing.
+  for (auto itemInfo : itemsDB) {
+    for (auto token : itemInfo) {
+      std::cout << token << " ";
+    }
+    std::cout << std::endl;
+  }
 }
 
 void Shop::shopItems() {
--- library/Shop.hpp
+++ library/Shop.hpp
@@ -1,11 +1,13 @@
 #ifndef SHOP_HPP
 #define SHOP_HPP
 
-#include <iostream>
+#include <string>
+#include <vector>
 
 class Shop {
   public:
     static void welcomeUser();
+    static std::vector<std::vector<std::string>> buildItemsDB();
     static void displayItems();
     static void shopItems();
     static void checkout();
